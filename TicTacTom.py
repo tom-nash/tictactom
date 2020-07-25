ccd7ecf83a86a25d8fb7cafe51adb86ade5d21ff
tom-nash <tom_nash1@outlook.com>
2020-07-25 16:45:09 +1000

    added quick perceptron network. How do we train something like this for Tic Tac Toe?

--- TicTacTom.py
+++ TicTacTom.py
@@ -1,22 +1,26 @@
 import numpy as np
 
-class NeuralNetwork():
-    def __init__(self):
+#A single layer perceptron network consisting of only one set of weights
+class PerceptronNeuralNetwork():
+    def __init__(self, x, y):
         #self.learning_rate = 1-6
         
         np.random.seed(1)
         
         #w, h matrix
-        #2 * the matric then -1 to get the mean of 0 and -1 < x <1
-        self.weights = 2 * np.random.random((3,1)) - 1
+        #2 * the matrix size, then -1 to get the mean of 0 and -1 < x <1
+        self.weights = 2 * np.random.random((x,y)) - 1
     
     #Normalise to 0<x<1
     def sigmoid(self, x):
         return 1 / (1+ np.exp(-x))
-
+    
+    #Calculate the derivative of sigmoid for cost calculation
     def sigmoidDerivative(self, x):
         return x * (1 - x)
 
+    #Iterate over samples, calculate a cost for each and tweak weights to
+    #converge on a minimum
     def train(self, training_inputs, training_labels, iterations):
         for iteration in range(iterations):
             output = self.think(training_inputs)
@@ -27,6 +31,7 @@ class NeuralNetwork():
 
             self.weights += cost
 
+    #Mult the input matrix with the weights to get the output
     def think(self, input):
         input = input.astype(float)
 
@@ -34,7 +39,8 @@ class NeuralNetwork():
         return output
 
 if __name__ == '__main__':
-        neural_network = NeuralNetwork()
+
+        neural_network = PerceptronNeuralNetwork(3,1)
 
         # The training set, with 4 examples consisting of 3
         # input values and 1 output value
