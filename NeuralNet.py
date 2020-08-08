5ae56c43f3b1ed51a3f2a3976c7433522fb14d57
Tom Nash <tom_nash1@outlook.com>
2020-08-08 14:19:38 +1000

    Working towards creating a single hidden layer neural network capable of playing tic tac toe with me. I'm so lonely

--- NeuralNet.py
+++ NeuralNet.py
@@ -3,18 +3,20 @@ import numpy as np
 #A single layer perceptron network consisting of only one set of weights
 class PerceptronNeuralNetwork():
     def __init__(self, x, y):
+        self.gameboard = None
+
         #self.learning_rate = 1-6
-        
+
         np.random.seed(1)
-        
+
         #w, h matrix
         #2 * the matrix size, then -1 to get the mean of 0 and -1 < x <1
         self.weights = 2 * np.random.random((x,y)) - 1
-    
+
     #Normalise to 0<x<1
     def sigmoid(self, x):
         return 1 / (1+ np.exp(-x))
-    
+
     #Calculate the derivative of sigmoid for cost calculation
     def sigmoidDerivative(self, x):
         return x * (1 - x)
@@ -23,25 +25,37 @@ class PerceptronNeuralNetwork():
     #converge on a minimum
     def train(self, training_inputs, training_labels, iterations):
         for iteration in range(iterations):
+
             output = self.think(training_inputs)
 
-            error = training_labels - output 
+            error = training_labels - output
 
-            cost = np.dot(training_inputs.T, error * self.sigmoidDerivative(output))
-
-            self.weights += cost
+            layeronecost = np.dot(training_inputs.T, error * self.sigmoidDerivative(output))
+            layertwocost = np.dot(training_inputs.T, error * self.sigmoidDerivative(output))
+            layerthreecost = np.dot(training_inputs.T, error * self.sigmoidDerivative(output))
+            self.layeroneweights += layeronecost
+            self.layertwoweights += layertwocost
+            self.layeroneweights += layerthreecost
 
     #Mult the input matrix with the weights to get the output
     def think(self, input):
         input = input.astype(float)
 
-        output = self.sigmoid(np.dot(input, self.weights))
+        #First layer activation function is sigmoid
+        first_layer_output = self.sigmoid(np.dot(input, self.layeroneweights))
+
+        second_layer_output = self.sigmoid(np.dot(first_layer_output, self.layertwoweights))
+
+        third_layer_output = self.sigmoid(np.dot(second_layer_output, self.layerthreeweights))
+
         return output
 
 if __name__ == '__main__':
 
         neural_network = PerceptronNeuralNetwork(3,1)
 
+
+        #The game boards 9x1 * No. of samples
         # The training set, with 4 examples consisting of 3
         # input values and 1 output value
         training_inputs = np.array([[0,0,1],
@@ -49,6 +63,8 @@ if __name__ == '__main__':
                                     [1,0,1],
                                     [0,1,1]])
 
+
+        #Wins/Losses
         training_outputs = np.array([[0,1,1,0]]).T
 
         # Train the neural network
@@ -57,7 +73,7 @@ if __name__ == '__main__':
         A = str(input("Input 1: "))
         B = str(input("Input 2: "))
         C = str(input("Input 3: "))
-        
+
         print("New situation: input data = ", A, B, C)
         print("Output data: ")
-        print(neural_network.think(np.array([A, B, C]))) 
+        print(neural_network.think(np.array([A, B, C])))
--- TicTacTom.py
+++ TicTacTom.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+from NeuralNet import PerceptronNeuralNetwork
+
 class gameboard():
     def __init__(self, seed=2):
         #np.random.seed(seed)
@@ -7,13 +9,13 @@ class gameboard():
         self.gamestate = np.full((3,3), 0) #Initialise the board to be all ' ' characters
         self.gameboard = np.full((3,3), ' ') #Initiaise the graphical board
         self.actionOnPlayer = np.random.choice([1,2]) #1 = Player, 2 = AI
-        
+
     def getPlayerStr(self):
         if self.actionOnPlayer == 1:
             return "X"
         else:
             return "O"
-    
+
     def switchPlayer(self):
         if self.actionOnPlayer == 1:
             self.actionOnPlayer = 2
@@ -40,8 +42,8 @@ class gameboard():
     def processTurn(self, x, y):
         #Check that the selection is a valid location
         if self.checkSpot(x,y):
-           self.gamestate[y-1][x-1] = self.actionOnPlayer  
-           self.gameboard[y-1][x-1] = self.getPlayerStr()  
+           self.gamestate[y-1][x-1] = self.actionOnPlayer
+           self.gameboard[y-1][x-1] = self.getPlayerStr()
            print(x,y," is a valid selection")
            return True
         else:
@@ -52,18 +54,20 @@ class gameboard():
         clearScrean()
         if self.debug:
             print(self.gamestate)
-        print(self.gameboard) 
+        print(self.gameboard)
 
     def checkSpot(self, x,y):
         if self.gamestate[y-1][x-1] == 1 or self.gamestate[y-1][x-1] == 2:
             return False
         else:
             return True
+
+    #Function to consider the board looking specifically for win conditions.
     def checkForWin(self):
         for indexes in win_indexes(3):
             if all(self.gamestate[r][c] == self.actionOnPlayer for r, c in indexes):
                 return True
-        return False 
+        return False
 
 def win_indexes(n):
     # Rows
@@ -76,19 +80,21 @@ def win_indexes(n):
     yield [(i, i) for i in range(n)]
     # Diagonal top right to bottom left
     yield [(i, n - 1 - i) for i in range(n)]
- 
+
 def clearScrean():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
     game = gameboard()
+    NeuralNetwork = PerceptronNeuralNetwork(9,9)
+    NeuralNetwork.gameboard = game
+
     clearScrean()
     print("Welcome to TicTacTom!\nThe first player is being chosen at random.")
+
     if game.actionOnPlayer is 1:
         print("The first player to take a turn is YOU")
     else:
         print("The AI gets the first turn")
 
     game.nextTurn()
-
-
